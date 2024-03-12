2d97cbc70145acf2fa60328dc129b97832c4f270
Ulyana <ushinkevich@mail.ru>
2024-03-12 13:38:01 +0400

    prog: task one

--- src/pages/taskOne/TaskOne.jsx
+++ src/pages/taskOne/TaskOne.jsx
@@ -1,7 +1,36 @@
+import s from './TaskOne.module.css';
+import { useState } from 'react';
+
 function TaskOne() {
+    const [text, setText] = useState('');
+
+    // Обработчик события для кнопки
+    const handleButtonClick = () => {
+        console.log('Кнопка нажата');
+    };
+
+    // Обработчик события для текстового поля
+    const handleTextChange = (event) => {
+        setText(event.target.value);
+    };
     return (
-        <div>
-            <header>TaskOne</header>
+        <div className={s.wrapper}>
+            <p className={s.header}>
+                Блок с кнопкой и текстовым полем. По нажатию на кнопку выполнить
+                запрос к https://catfact.ninja/fact. Полученный факт нужно
+                записать в текстовое поле и установить курсор после первого
+                слова.{' '}
+            </p>
+            <input
+                type='text'
+                value={text}
+                onChange={handleTextChange}
+                placeholder='Введите текст'
+                className={s.input}
+            />
+            <button className={s.btn} onClick={handleButtonClick}>
+                Нажми на меня
+            </button>
         </div>
     );
 }
