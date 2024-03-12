24b662b77af21c75a6c45d07439e830108cbb344
Ulyana <ushinkevich@mail.ru>
2024-03-12 13:10:08 +0400

    feat: nav btn main page

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,7 +1,19 @@
+import s from './Main.module.css';
+import { useNavigate } from 'react-router';
+
 function Main() {
+    const navigate = useNavigate();
     return (
         <div>
-            <header>Main</header>
+            <header>Select the task number to check</header>
+            <div className={s.nav}>
+                <button className={s.btn} onClick={() => navigate('/taskOne')}>
+                    Task №1
+                </button>
+                <button className={s.btn} onClick={() => navigate('/taskTwo')}>
+                    Task №2
+                </button>
+            </div>
         </div>
     );
 }
