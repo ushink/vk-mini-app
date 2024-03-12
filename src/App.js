b859c1c1ab962327212cb01df57cb69953100196
Ulyana <ushinkevich@mail.ru>
2024-03-12 08:05:42 +0400

    start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">Hello, word!</header>
+        </div>
+    );
+}
+
+export default App;
