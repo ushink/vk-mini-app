2ddff8e32d5c837e3ddec13a191c1836da4a6f88
Ulyana <ushinkevich@mail.ru>
2024-03-13 07:39:50 +0400

    feat: layout task 2

--- src/pages/taskTwo/TaskTwo.jsx
+++ src/pages/taskTwo/TaskTwo.jsx
@@ -1,7 +1,30 @@
+/* eslint-disable no-unused-vars */
+import s from './TaskTwo.module.css';
+import { useState } from 'react';
+
 function TaskTwo() {
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
-        <div>
-            <header>TaskTwo</header>
+        <div className={s.wrapper}>
+            <form className={s.form} onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder='Введите ваше имя'
+                    className={s.input}
+                />
+                <button className={s.btn} type='submit'>
+                    Отправить
+                </button>
+            </form>
+            {age && <span className={s.text}>Ваш возраст: {age}</span>}
         </div>
     );
 }
